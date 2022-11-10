e65f99659c2eb1eb863a07d5605b56b02c1f3e46
Bitvinus <dezenu@yandex.ru>
2022-11-10 17:01:18 +0300

    fix: some refactoring

---
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,14 @@
+import { View } from 'react-native';
+import ExpensesList from './ExpensesList';
+import ExpensesSummary from './ExpensesSummary';
+
+function ExpensesOutput({ expenses, expensesPeriod }) {
+  return (
+    <View>
+      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      <ExpensesList />
+    </View>
+  );
+}
+
+export default ExpensesOutput;
