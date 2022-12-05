ce17afbda9298b86abd10d525ef675a3fd975a28
Bitvinus <dezenu@yandex.ru>
2022-12-05 11:33:05 +0300

    feat: add updating and deleting from firebase

--- util/http.js
+++ util/http.js
@@ -3,8 +3,13 @@ import axios from 'axios';
 const BACKEND_URL =
   'https://expenses-project-5f455-default-rtdb.firebaseio.com';
 
-export function storeExpense(expenseData) {
-  axios.post(BACKEND_URL + '/expenses.json', expenseData);
+export async function storeExpense(expenseData) {
+  const response = await axios.post(
+    BACKEND_URL + '/expenses.json',
+    expenseData,
+  );
+  const id = response.data.name;
+  return id;
 }
 
 export async function fetchExpenses() {
@@ -16,10 +21,18 @@ export async function fetchExpenses() {
     const expenseObj = {
       id: key,
       amount: response.data[key].amount,
-      date: response.data[key].date,
+      date: new Date(response.data[key].date),
       description: response.data[key].description,
     };
     expenses.push(expenseObj);
   }
   return expenses;
+}
+
+export function updateExpense(id, expenseData) {
+  return axios.put(BACKEND_URL + `/expenses/${id}.json`, expenseData);
+}
+
+export function deleteExpense(id) {
+  return axios.delete(BACKEND_URL + `/expenses/${id}.json`);
 }
